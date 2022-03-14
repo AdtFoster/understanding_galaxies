2903276abf00f37d70ac271e2d7811b367b71946
Mike Walmsley <walmsleymk1@gmail.com>
2022-03-14 17:43:42 +0000

    make predictions on images so far

--- cluster_command.sh
+++ cluster_command.sh
@@ -59,23 +59,23 @@ DELTA_P=0.016
 DELTA_MAG=0.5
 # TODO specify DELTA_MASS
 
-$PYTHON $THIS_REPO_DIR/creating_images_semester_two.py \
-    --fits-dir $FITS_DIR \
-    --save-dir $SCALED_IMG_DIR \
-    --max-redshift $MAX_Z \
-    --step-size $STEP_SIZE
+# $PYTHON $THIS_REPO_DIR/creating_images_semester_two.py \
+#     --fits-dir $FITS_DIR \
+#     --save-dir $SCALED_IMG_DIR \
+#     --max-redshift $MAX_Z \
+#     --step-size $STEP_SIZE
 
-# $PYTHON $THIS_REPO_DIR/make_predictions.py \
-#     --batch-size 256 \
-#     --input-dir $SCALED_IMG_DIR \
-#     --checkpoint-loc /share/nas2/walml/repos/gz-decals-classifiers/results/tensorflow/all_campaigns_ortho_v2_train_only_m0/checkpoint \
-#     --save-loc /share/nas2/walml/repos/understanding_galaxies/results/scaled_image_predictions.csv
+$PYTHON $THIS_REPO_DIR/make_predictions.py \
+    --batch-size 256 \
+    --input-dir $SCALED_IMG_DIR \
+    --checkpoint-loc /share/nas2/walml/repos/gz-decals-classifiers/results/tensorflow/all_campaigns_ortho_v2_train_only_m0/checkpoint \
+    --save-loc /share/nas2/walml/repos/understanding_galaxies/results/scaled_image_predictions.hdf5
 
 #  load predictions in convenient dataframe
-# $PYTHON $THIS_REPO_DIR/create_dataframe.py \
-#     --file-name /share/nas2/walml/repos/understanding_galaxies/results/scaled_image_predictions.hdf5 \
-#     --min-allow-z $MIN_ALLOW_Z \
-#     --max-allow-z $MAX_ALLOW_Z
+$PYTHON $THIS_REPO_DIR/create_dataframe.py \
+    --file-name /share/nas2/walml/repos/understanding_galaxies/results/scaled_image_predictions.hdf5 \
+    --min-allow-z $MIN_ALLOW_Z \
+    --max-allow-z $MAX_ALLOW_Z
 
 # # apply debiasing method, to each galaxy, by sampling nearby galaxies
 # $PYTHON $THIS_REPO_DIR/sampling_galaxies.py \
