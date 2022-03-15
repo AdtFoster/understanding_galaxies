66c647e179f42eacc5a71b6262a50e303770bd94
Mike Walmsley <walmsleymk1@gmail.com>
2022-03-15 10:28:51 +0000

    try create df from many chunks

--- cluster_command.sh
+++ cluster_command.sh
@@ -65,17 +65,17 @@ DELTA_MAG=0.5
 #     --max-redshift $MAX_Z \
 #     --step-size $STEP_SIZE
 
-$PYTHON $THIS_REPO_DIR/make_predictions.py \
-    --batch-size 256 \
-    --image-dir $SCALED_IMG_DIR \
-    --checkpoint-loc /share/nas2/walml/repos/gz-decals-classifiers/results/tensorflow/all_campaigns_ortho_v2_train_only_m0/checkpoint \
-    --save-dir /share/nas2/walml/repos/understanding_galaxies/results/latest_scaled_predictions
+# $PYTHON $THIS_REPO_DIR/make_predictions.py \
+#     --batch-size 256 \
+#     --image-dir $SCALED_IMG_DIR \
+#     --checkpoint-loc /share/nas2/walml/repos/gz-decals-classifiers/results/tensorflow/all_campaigns_ortho_v2_train_only_m0/checkpoint \
+#     --save-dir /share/nas2/walml/repos/understanding_galaxies/results/latest_scaled_predictions
 
-# #  load predictions in convenient dataframe
-# $PYTHON $THIS_REPO_DIR/create_dataframe.py \
-#     --predictions-dir /share/nas2/walml/repos/understanding_galaxies/results/latest_scaled_predictions \
-#     --min-allow-z $MIN_ALLOW_Z \
-#     --max-allow-z $MAX_ALLOW_Z
+#  load predictions in convenient dataframe
+$PYTHON $THIS_REPO_DIR/create_dataframe.py \
+    --predictions-dir /share/nas2/walml/repos/understanding_galaxies/results/latest_scaled_predictions \
+    --min-allow-z $MIN_ALLOW_Z \
+    --max-allow-z $MAX_ALLOW_Z
 
 # # apply debiasing method, to each galaxy, by sampling nearby galaxies
 # $PYTHON $THIS_REPO_DIR/sampling_galaxies.py \
