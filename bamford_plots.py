405b347456ad50f7fe63126ea910718f04353666
Adam Foster <adam.foster@student.manchester.ac.uk>
2022-04-28 16:35:29 +0100

    Update to bamford plots
    Added extra plots for sim, db, db with sim pop, and sim from bamford data

--- bamford_plots.py
+++ bamford_plots.py
@@ -19,13 +19,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--threshold-val', dest='threshold_val', type=float)
     parser.add_argument('--rounding', dest='rounding', type=float)
-    parser.add_argument('--predictions-dir', dest='predictions_dir', type=str)
+    parser.add_argument('--prediction-dir', dest='prediction_dir', type=str)
     
     args = parser.parse_args()
     
-    logging.info('MORPHOLOGY WITH REDSHIFT CODE COMMENCING')
+    logging.info('BAMFORD PLOTTING COMMENCING')
     
-    predictions_dir = args.predictions_dir
+    predictions_dir = args.prediction_dir
     
     full_data = pd.read_csv('output_csvs/full_data_1m_with_resizing.csv') #index_col=0
     full_data = full_data.sort_values(by = 'redshift')
@@ -72,61 +72,157 @@ if __name__ == '__main__':
             df_redshift = pd.DataFrame([[redshift, confident_smooth/total, confident_featured/total, confident_artifact/total]], columns=df.columns)
             df = pd.concat([df,df_redshift])
         
-        frf.error_bar_smoothness_3(df['redshift'].values, df['smooth'].values, df['featured'].values, df['artifact'].values, save_name=figure_save_names[i], title='Galaxy Morphology with Redshift for Magnitudes {0}'.format(figure_mag_lims[i]), xlabel='Redshift', ylabel='Proportion of expected predictions', ylimits=[0, 1], xlimits=[0, 0.12])
+        frf.error_bar_smoothness_3(df['redshift'].values, df['smooth'].values, df['featured'].values, df['artifact'].values, save_name=figure_save_names[i], title='Simulated Galaxy Morphologies with Redshift for Magnitudes {0}'.format(figure_mag_lims[i]), xlabel='Redshift', ylabel='Proportion of expected predictions', ylimits=[0, 1], xlimits=[0, 0.12])
         i+=1
     
     logging.info("Original plots plotted")
     
+###BEGINING THE PLOTTING OF DEBIASED GALAXIES TREATED AS INDEPENDANT GALAXIES###
+
     figure_save_names = ['other_plots/smoothness_cut_18_20_{0}_graph_redshift_certain_classification_extended_data_remade_debiased.png'.format(cut_threshold), 
                         'other_plots/smoothness_cut_20_21_{0}_graph_redshift_certain_classification_extended_data_remade_debiased.png'.format(cut_threshold),
                         'other_plots/smoothness_cut_21_24_{0}_graph_redshift_certain_classification_extended_data_remade_debiased.png'.format(cut_threshold)]
     
     debiased_df_chunks = [pd.read_csv(loc) for loc in glob.glob(os.path.join(predictions_dir, '*.csv'))]
     debiased_df = pd.concat(debiased_df_chunks, axis=0).reset_index(drop=True)
+    merged_dataframe = debiased_df
     
-    if os.path.isdir('/share/nas2'):
-        catalog_loc = '/share/nas2/walml/repos/gz-decals-classifiers/data/catalogs/nsa_v1_0_1_mag_cols.parquet'
-    else:
-        catalog_loc = '/Users/adamfoster/Documents/MPhysProject/understanding_galaxies/making_figures/nsa_v1_0_1_mag_cols.parquet'
-    
-    nsa_catalog = pd.read_parquet(catalog_loc, columns=['iauname', 'elpetro_absmag_r'])
-    
-    merged_dataframe = debiased_df.merge(nsa_catalog, on='iauname', how='inner')
-    
-    first_mag_cut = merged_dataframe[(merged_dataframe["elpetro_absmag_r"] < -18 ) & (merged_dataframe["elpetro_absmag_r"] >= -20)]
-    first_mag_cut['redshift'] = np.round(first_mag_cut["redshift"] * (1/rounding))/(1/rounding)
-    second_mag_cut = merged_dataframe[(merged_dataframe["elpetro_absmag_r"] < -20 ) & (merged_dataframe["elpetro_absmag_r"] >= -21)]
-    second_mag_cut['redshift'] = np.round(second_mag_cut["redshift"] * (1/rounding))/(1/rounding)
-    third_mag_cut = merged_dataframe[(merged_dataframe["elpetro_absmag_r"] < -21 ) & (merged_dataframe["elpetro_absmag_r"] >= -24)]
-    third_mag_cut['redshift'] = np.round(third_mag_cut["redshift"] * (1/rounding))/(1/rounding)
-    
+    first_mag_cut = merged_dataframe[(merged_dataframe["magnitude"] < -18 ) & (merged_dataframe["magnitude"] >= -20)].reset_index(drop = True)
+    first_mag_cut['max_z'] = np.round(first_mag_cut["max_z"] * (1/rounding))/(1/rounding)
+    second_mag_cut = merged_dataframe[(merged_dataframe["magnitude"] < -20 ) & (merged_dataframe["magnitude"] >= -21)].reset_index(drop = True)
+    second_mag_cut['max_z'] = np.round(second_mag_cut["max_z"] * (1/rounding))/(1/rounding)
+    third_mag_cut = merged_dataframe[(merged_dataframe["magnitude"] < -21 ) & (merged_dataframe["magnitude"] >= -24)].reset_index(drop = True)
+    third_mag_cut['max_z'] = np.round(third_mag_cut["max_z"] * (1/rounding))/(1/rounding)
+
     j = 0
     for cut in [first_mag_cut, second_mag_cut, third_mag_cut]:
         df = pd.DataFrame(columns=['redshift','smooth','featured','artifact'])
-        redshifts = pd.unique(cut['redshift'])
+        redshifts = pd.unique(cut['max_z'])
+        redshifts.sort()
         for pred_redshift in redshifts:
             
             count_smooth = [] #list for each weighted mean prediction (smooth prediction)
             count_featured = [] #list for each weighted mean prediction (featured prediction)
             count_artifact = [] #list for each weighted mean prediction (artifact prediction)
             
-            for i in range(len(cut)):
-                if cut['debiased_smooth_pred'][i]>=cut_threshold:
+            cut_at_redshift=cut[cut['max_z']==pred_redshift].reset_index(drop = True)
+
+            for i in range(len(cut_at_redshift)):
+                if cut_at_redshift['debiased_smooth_pred'][i]>=cut_threshold:
                     count_smooth.append("smooth")
-                elif cut['debiased_featured_pred'][i]>=cut_threshold:
+                elif cut_at_redshift['debiased_featured_pred'][i]>=cut_threshold:
                     count_featured.append("featured")
-                elif cut['debiased_artifact_pred'][i]>=cut_threshold:
+                elif cut_at_redshift['debiased_artifact_pred'][i]>=cut_threshold:
                     count_artifact.append("artifact")
+                else:
+                    pass
                             
             confident_smooth = len(count_smooth) 
             confident_featured = len(count_featured)
             confident_artifact = len(count_artifact)
             total = confident_smooth + confident_featured + confident_artifact
-            df_redshift = pd.DataFrame([[pred_redshift, confident_smooth/total, confident_featured/total, confident_artifact/total]], columns=df.columns)
-            df = pd.concat([df,df_redshift])
+
+            if total>5:
+                df_redshift = pd.DataFrame([[pred_redshift, confident_smooth/total, confident_featured/total, confident_artifact/total]], columns=df.columns)
+                df = pd.concat([df,df_redshift])
             
-        frf.error_bar_smoothness_3(df['redshift'].values, df['smooth'].values, df['featured'].values, df['artifact'].values, save_name=figure_save_names[j], title='Galaxy Morphology with Redshift for Magnitudes {0}'.format(figure_mag_lims[j]), xlabel='Redshift', ylabel='Proportion of expected predictions', ylimits=[0, 1], xlimits=[0, 0.12])
+        frf.error_bar_smoothness_3(df['redshift'].values, df['smooth'].values, df['featured'].values, df['artifact'].values, save_name=figure_save_names[j], title='De-biased Galaxy Morphology with Redshift for Magnitudes {0}'.format(figure_mag_lims[j]), xlabel='Redshift', ylabel='Proportion of expected predictions', ylimits=[0, 1], xlimits=[0, 0.12])
         j += 1
         logging.info("new cut")
         
+
+    ###BEGINNING THE PLOTTING OF DEBIASED PREDICTIONS USING SAME POPULATION AS NON DEBIASED###
+    
+    logging.info("2nd plots plotted")
+
+    figure_save_names = ['other_plots/smoothness_cut_18_20_{0}_graph_redshift_certain_classification_debiased_nondbpop.png'.format(cut_threshold), 
+                        'other_plots/smoothness_cut_20_21_{0}_graph_redshift_certain_classification_debiased_nondbpop.png'.format(cut_threshold),
+                        'other_plots/smoothness_cut_21_24_{0}_graph_redshift_certain_classification_debiased_nondbpop.png'.format(cut_threshold)]
+
+    j = 0
+    for cut in [first_mag_cut, second_mag_cut, third_mag_cut]:
+        df = pd.DataFrame(columns=['redshift','smooth','featured','artifact'])
+        redshifts = pd.unique(cut['max_z'])
+        redshifts.sort()
+        for pred_redshift in redshifts:
+            
+            count_smooth = [] #list for each weighted mean prediction (smooth prediction)
+            count_featured = [] #list for each weighted mean prediction (featured prediction)
+            count_artifact = [] #list for each weighted mean prediction (artifact prediction)
+            
+            cut_at_redshift=cut[cut['max_z']==pred_redshift].reset_index(drop = True)
+            at_redshift_smooth=cut_at_redshift[cut_at_redshift['actual_smooth_pred']>=cut_threshold]
+            at_redshift_featured=cut_at_redshift[cut_at_redshift['actual_featured_pred']>=cut_threshold]
+            at_redshift_artifact=cut_at_redshift[cut_at_redshift['actual_artifact_pred']>=cut_threshold]
+            cut_at_redshift=pd.concat([at_redshift_smooth, at_redshift_featured, at_redshift_artifact]).reset_index(drop = True)
+
+            for i in range(len(cut_at_redshift)):
+                if cut_at_redshift['debiased_smooth_pred'][i]>=cut_threshold:
+                    count_smooth.append("smooth")
+                elif cut_at_redshift['debiased_featured_pred'][i]>=cut_threshold:
+                    count_featured.append("featured")
+                elif cut_at_redshift['debiased_artifact_pred'][i]>=cut_threshold:
+                    count_artifact.append("artifact")
+                else:
+                    pass
+                            
+            confident_smooth = len(count_smooth) 
+            confident_featured = len(count_featured)
+            confident_artifact = len(count_artifact)
+            total = confident_smooth + confident_featured + confident_artifact
+
+            if total>5:
+                df_redshift = pd.DataFrame([[pred_redshift, confident_smooth/total, confident_featured/total, confident_artifact/total]], columns=df.columns)
+                df = pd.concat([df,df_redshift])
+            
+        frf.error_bar_smoothness_3(df['redshift'].values, df['smooth'].values, df['featured'].values, df['artifact'].values, save_name=figure_save_names[j], title='Galaxy Morphology with Redshift for Magnitudes {0} Using Non-de-biased Population'.format(figure_mag_lims[j]), xlabel='Redshift', ylabel='Proportion of expected predictions', ylimits=[0, 1], xlimits=[0, 0.12])
+        j += 1
+        logging.info("new cut")
+
+    logging.info("3rd plots plotted")
+
+    ###BEGINNING THE PLOTTING OF NON DEBIASED USING BAMFORD DATA###  
+
+    figure_save_names = ['other_plots/smoothness_cut_18_20_{0}_graph_redshift_certain_classification_nondb_bamford.png'.format(cut_threshold), 
+                        'other_plots/smoothness_cut_20_21_{0}_graph_redshift_certain_classification_nondb_bamford.png'.format(cut_threshold),
+                        'other_plots/smoothness_cut_21_24_{0}_graph_redshift_certain_classification_nondb_bamford.png'.format(cut_threshold)]
+
+    j = 0
+    for cut in [first_mag_cut, second_mag_cut, third_mag_cut]:
+        df = pd.DataFrame(columns=['redshift','smooth','featured','artifact'])
+        redshifts = pd.unique(cut['max_z'])
+        redshifts.sort()
+        for pred_redshift in redshifts:
+            
+            count_smooth = [] #list for each weighted mean prediction (smooth prediction)
+            count_featured = [] #list for each weighted mean prediction (featured prediction)
+            count_artifact = [] #list for each weighted mean prediction (artifact prediction)
+            
+            cut_at_redshift=cut[cut['max_z']==pred_redshift].reset_index(drop = True)
+
+            for i in range(len(cut_at_redshift)):
+                if cut_at_redshift['sim_smooth_pred'][i]>=cut_threshold:
+                    count_smooth.append("smooth")
+                elif cut_at_redshift['sim_featured_pred'][i]>=cut_threshold:
+                    count_featured.append("featured")
+                elif cut_at_redshift['sim_artifact_pred'][i]>=cut_threshold:
+                    count_artifact.append("artifact")
+                else:
+                    pass
+                            
+            confident_smooth = len(count_smooth) 
+            confident_featured = len(count_featured)
+            confident_artifact = len(count_artifact)
+            total = confident_smooth + confident_featured + confident_artifact
+
+            if total>5:
+                df_redshift = pd.DataFrame([[pred_redshift, confident_smooth/total, confident_featured/total, confident_artifact/total]], columns=df.columns)
+                df = pd.concat([df,df_redshift])
+            
+        frf.error_bar_smoothness_3(df['redshift'].values, df['smooth'].values, df['featured'].values, df['artifact'].values, save_name=figure_save_names[j], title='Simulated Galaxy Morphology with Redshift for Magnitudes {0}'.format(figure_mag_lims[j]), xlabel='Redshift', ylabel='Proportion of expected predictions', ylimits=[0, 1], xlimits=[0, 0.12])
+        j += 1
+        logging.info("new cut")
+
+    logging.info("4th plots plotted")
+
     logging.info('\n MORPHOLOGY WITH REDSHIFT CODE COMPLETE')
